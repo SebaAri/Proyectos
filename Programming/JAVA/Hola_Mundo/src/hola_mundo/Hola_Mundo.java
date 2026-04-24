87323e3f7c0b3e69401baf095f02e4a25d24971a
Ariel <155785268+SebaAri@users.noreply.github.com>
2026-04-23 21:30:27 -0300

    Elimino una carpeta

--- Programming/JAVA/Hola_Mundo/src/hola_mundo/Hola_Mundo.java
+++
@@ -1,7 +0,0 @@
-package hola_mundo;
-public class Hola_Mundo {
-public static void main(String[] args) {
-       System.out.println ("Hola mundo, bienvenido al curso java nivel básico");
-    }
-    
-}
